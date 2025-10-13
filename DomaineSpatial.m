0f4d1081e39619b2bb1d979253e110d1abd5ee75
vercors04 <xavkrs@gmail.com>
2025-10-13 10:54:31 +0200

    changement fct...

--- DomaineSpatial.m
+++ DomaineSpatial.m
@@ -1,1 +1,7 @@
 function [ds,s,Ns]=DomaineSpatial(Lamb,L)
+	% Domaine spatial
+	ds=R;         % Pas en espace [m]
+	s=0:ds:L;       % Echantillonage spatial [m]
+	Ns=length(s);   % Nombre de points d'échantillonages en espace
+
+
--- DomaineTemporel.m
+++ DomaineTemporel.m
@@ -1,1 +1,6 @@
 function [dt,t,Nt,tmax]=DomaineTemporel(Per,L)
+	% Domaine temporel
+	dt=2*pi/(wmax/2);   % Pas en temps [s]
+	tmax=2*pi/(dw);% Temps maximum de la simulation [s]
+	t=0:dt:tmax;    % Echantillonage temporel [s]
+	Nt=length(t);   % Nombre de points d'échantillonages en temps
